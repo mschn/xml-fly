b569c741fd175926b5a4d97c209b955c21a20169
mathieu schnoor <mathieu.schnoor@gmail.com>
2019-01-10 22:58:41 +0100

    change font library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,6 @@ import { AppComponent } from './app.component';
 import { XmlViewComponent } from './xml-view/xml-view.component';
 import { NodeComponent } from './node/node.component';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlusSquare, faMinusSquare, faFolderOpen } from '@fortawesome/free-regular-svg-icons';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,15 +12,12 @@ import { faCode } from '@fortawesome/free-solid-svg-icons';
     NodeComponent
   ],
   imports: [
-    BrowserModule,
-    FontAwesomeModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    library.add(faPlusSquare, faMinusSquare, faFolderOpen, faCode);
-  }
+  constructor() {}
 
 }
