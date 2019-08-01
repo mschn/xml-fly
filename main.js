4409816af8d9dde4ba8e958ceef3d0accdb6d51e
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2019-08-01 17:57:32 +0200

    add an icon to the electron build

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const url = require("url");
+const shell = require('electron').shell;
 
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, '/dist/assets/icons/32x32.png')
+  });
 
   // load the dist folder from Angular
   win.loadURL(
@@ -15,6 +20,13 @@ function createWindow() {
       slashes: true
     })
   );
+
+  win.setMenuBarVisibility(false);
+
+  win.webContents.on('new-window', (event, url) => {
+    event.preventDefault()
+    shell.openExternal(url)
+  });
 
   // The following is optional and will open the DevTools:
   // win.webContents.openDevTools()
