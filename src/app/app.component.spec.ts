9a07b1a6265879d9447263dd978b2fa3399a4c70
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2020-07-18 23:13:12 +0200

    build: npm audit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HotkeyModule } from 'angular2-hotkeys';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HotkeyModule.forRoot()
+      ],
       declarations: [
         AppComponent
       ],
+      providers: []
     }).compileComponents();
   }));
 
